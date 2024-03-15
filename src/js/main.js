e9e52a63c0bc6676e96a4cb364d6f321dd76ada9
Emma Larsson <emma940112@gmail.com>
2024-03-15 01:23:58 +0100

    Har lagt till en funktion som gör att man kan klicka på knapparna med skådespelarnas namn och då få upp olika filmer som de är kända för.

--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,7 @@ function checkInput() {
 async function searchMovie(movie) {
     try {
         //Fetch-anrop
-        const response = await fetch(`http://www.omdbapi.com/?t=${movie}&plot=full&apikey=d68ae12b`);
+        const response = await fetch(`https://www.omdbapi.com/?t=${movie}&plot=full&apikey=d68ae12b`);
         const data = await response.json();
 
         if (data.Response === "True") {
@@ -38,23 +38,68 @@ async function searchMovie(movie) {
             //Variabel för resultat1
             const result1El = document.getElementById("result1");
             const result2El = document.getElementById("result2");
+            const result3El = document.getElementById("result3");
 
             //Skriv ut bild, titel och beskrivning till resultat1
             result1El.innerHTML = `<img src=${movieImage}><h2>${movieTitle}</h2><p>${movieGenre}</p><p>${moviePlot}</p>`;
 
+            //Ta bort tidigare knappar
+            result2El.innerHTML = "";
+
+            //Ta bort tidigare sökresultat
+            result3El.innerHTML = "";
+
+            //Skriv ut skådespelarna i form av knappar
             movieActors.forEach((actor, index) => {
                 const actorBtn = document.createElement("button");
                 actorBtn.textContent = actor;
                 const btnId = `${index}`;
                 actorBtn.id = btnId;
                 result2El.appendChild(actorBtn);
+
+                //Händelsehanterare till knapparna för skådespelarna
+                actorBtn.addEventListener("click", actorSearch);
             });
 
         } else {
-            console.error(error);
+            console.error("Something went wrong.", error);
+            document.getElementById("error").innerHTML = "<p>Something went wrong, please try again later.</p>";
         }
 
     } catch {
-        document.getElementById("error").innerHTML = "<p>Something went wrong, please try again later.</p>"
+        console.error("Something went wrong.", error);
+        document.getElementById("error").innerHTML = "<p>Something went wrong, please try again later.</p>";
+    }
+}
+
+//Funktion till knapparna för skådespelarna
+async function actorSearch() {
+    //Hämta namnet på skådespelaren från knappen
+    const actor = this.textContent;
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/search/person?api_key=5980c8441267fa0280955a5da15fe080&query=${actor}`);
+        const data = await response.json();
+
+        if (data.Response = "True") {
+            //Variabler
+            const movies = data.results[0].known_for;
+            const result3El = document.getElementById("result3");
+
+            ///Ta bort tidigare resultat
+            result3El.innerHTML = "";
+
+            movies.forEach(movie => {
+                const actorMovieTitle = movie.title;
+
+                result3El.innerHTML += `<h2>${actorMovieTitle}</h2>`;
+            });
+        } else {
+            console.error("Something went wrong.", error);
+            document.getElementById("error").innerHTML = "<p>Something went wrong, please try again later.</p>";
+        }
+
+    } catch (error) {
+        console.error("Something went wrong.", error);
+        document.getElementById("error").innerHTML = "<p>Something went wrong, please try again later.</p>";
     }
 }
